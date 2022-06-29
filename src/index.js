c9c3b082a3f5601753cba89b882fe3d7a2bc30db
Patrice-H <https://github.com/Patrice-H>
2022-06-30 00:09:58 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './components/App';
+//import App from './components/App';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
