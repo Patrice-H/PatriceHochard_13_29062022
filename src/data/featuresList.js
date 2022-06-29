5097a44aa105840acdbc603abbbbc0335d1808f4
Patrice-H <https://github.com/Patrice-H>
2022-06-30 00:09:21 +0200

    Add a features list data

---
+++ src/data/featuresList.js
@@ -0,0 +1,27 @@
+import iconChat from '../assets/icon-chat.png';
+import iconMoney from '../assets/icon-money.png';
+import iconSecurity from '../assets/icon-security.png';
+
+export const featuresList = [
+  {
+    id: 'chat01',
+    src: iconChat,
+    alt: 'Chat Icon',
+    title: 'You are our #1 priority',
+    text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+  },
+  {
+    id: 'money02',
+    src: iconMoney,
+    alt: 'Money Icon',
+    title: 'More savings means higher rates',
+    text: 'The more you save with us, the higher your interest rate will be!',
+  },
+  {
+    id: 'security03',
+    src: iconSecurity,
+    alt: 'Security Icon',
+    title: 'Security you can trust',
+    text: 'We use top of the line encryption to make sure your data and money is always safe.',
+  },
+];
