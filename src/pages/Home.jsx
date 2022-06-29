1e51479f0ec62bf35d075e071e559d390d81ac16
Patrice-H <https://github.com/Patrice-H>
2022-06-30 00:10:35 +0200

    Add a home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import Feature from '../components/Feature';
+import { featuresList } from '../data/featuresList';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <div className="hero">
+          <section className="hero-content">
+            <h2 className="sr-only">Promoted Content</h2>
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+            <p className="text">
+              Open a savings account with Argent Bank today!
+            </p>
+          </section>
+        </div>
+        <section className="features">
+          <h2 className="sr-only">Features</h2>
+          {featuresList.map((feature) => (
+            <Feature
+              key={feature.id}
+              src={feature.src}
+              alt={feature.alt}
+              title={feature.title}
+              text={feature.text}
+            />
+          ))}
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
