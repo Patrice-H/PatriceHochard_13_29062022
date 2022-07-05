f6b1e91a6b90535114266c30ffc3b4df84bfaf07
Patrice-H <webmestre64@gmail.com>
2022-07-05 11:11:52 +0200

    Add a route to user page and provider to manage store in app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-//import App from './components/App';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import User from './pages/User';
+import './index.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/user" element={<User />}></Route>
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>
 );
