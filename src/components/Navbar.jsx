451a876efdd9afd886039cb3006824e5c7ec52ff
Patrice-H <webmestre64@gmail.com>
2022-07-05 13:44:44 +0200

    Add a signout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/argentBankLogo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToken, toggleLoginLogout } from '../features/login/loginSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const logoutUser = () => {
+    dispatch(addToken(null));
+    dispatch(toggleLoginLogout());
+  };
+
+  const isUserLogedIn = useSelector((state) => state.login.isUserLogedIn);
+
   return (
     <nav className="main-nav">
       <Link to="/" className="main-nav-logo">
@@ -13,10 +24,17 @@ const Navbar = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link to="/signin" className="main-nav-item">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
+        {isUserLogedIn ? (
+          <Link to="/" className="main-nav-item" onClick={logoutUser}>
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </Link>
+        ) : (
+          <Link to="/signin" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
