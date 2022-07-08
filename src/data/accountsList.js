285eca13bdfcbccb4e5d2a924eb4e59d946d879b
Patrice-H <webmestre64@gmail.com>
2022-07-08 17:04:00 +0200

    Add an account list as data

---
+++ src/data/accountsList.js
@@ -0,0 +1,20 @@
+export const accountsList = [
+  {
+    id: 'checking-01',
+    title: 'Argent Bank Checking (x8349)',
+    amount: 2082.79,
+    description: 'Available Balance',
+  },
+  {
+    id: 'savings-02',
+    title: 'Argent Bank Savings (x6712)',
+    amount: 10928.42,
+    description: 'Available Balance',
+  },
+  {
+    id: 'credit-card-03',
+    title: 'Argent Bank Credit Card (x8349)',
+    amount: 184.3,
+    description: 'Current Balance',
+  },
+];
