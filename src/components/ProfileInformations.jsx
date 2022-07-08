675eb734efd0e7c1c060ed861141ff2005b8d221
Patrice-H <webmestre64@gmail.com>
2022-07-08 17:06:46 +0200

    Add a component to display user information on profile page

---
+++ src/components/ProfileInformations.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { setEditMode } from '../features/profile/profileSlice';
+
+const ProfileInformations = () => {
+  const firstName = useSelector((state) => state.profile.firstName);
+  const lastName = useSelector((state) => state.profile.lastName);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="header">
+      <h1>
+        <span>Welcome back</span>
+        <br />
+        <span>
+          {firstName && firstName}
+          &nbsp;
+          {lastName && lastName}
+        </span>
+      </h1>
+      <button
+        className="dark-button info-button"
+        onClick={() => {
+          dispatch(setEditMode(true));
+        }}
+      >
+        Edit Name
+      </button>
+    </div>
+  );
+};
+
+export default ProfileInformations;
