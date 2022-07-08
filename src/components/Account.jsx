e95cb07e6226b1f236e7c15535c20504ff89eb3b
Patrice-H <webmestre64@gmail.com>
2022-07-08 17:02:57 +0200

    Add an account component

---
+++ src/components/Account.jsx
@@ -0,0 +1,23 @@
+const Account = ({ title, amount, description }) => {
+  const formatAmount = () => {
+    return new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(amount);
+  };
+
+  return (
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{formatAmount()}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
+  );
+};
+
+export default Account;
