275bce41d30df36a80208f0d15f901a5d1116c9f
Patrice-H <webmestre64@gmail.com>
2022-07-09 14:31:37 +0200

    Update profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,15 +5,20 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import userProfile from '../services/userProfile';
 import { getFirstName, getLastName } from '../features/profile/profileSlice';
+import Account from '../components/Account';
+import { accountsList } from '../data/accountsList';
+import ProfileInformations from '../components/ProfileInformations';
+import ProfileEditor from '../components/ProfileEditor';
 
 const Profile = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const token = useSelector((state) => state.login.token);
   const isUserLogedIn = useSelector((state) => state.login.isUserLogedIn);
-
   const firstName = useSelector((state) => state.profile.firstName);
   const lastName = useSelector((state) => state.profile.lastName);
+  const editMode = useSelector((state) => state.profile.editMode);
 
   useEffect(() => {
     if (!isUserLogedIn) {
@@ -36,47 +41,16 @@ const Profile = () => {
     <>
       <Navbar />
       <main className="main bg-dark">
-        <div className="header">
-          <h1>
-            Welcome back
-            <br />
-            {firstName && firstName}
-            &nbsp;
-            {lastName && lastName}
-          </h1>
-          <button className="edit-button">Edit Name</button>
-        </div>
+        {editMode ? <ProfileEditor /> : <ProfileInformations />}
         <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
+        {accountsList.map((account) => (
+          <Account
+            key={account.id}
+            title={account.title}
+            amount={account.amount}
+            description={account.description}
+          />
+        ))}
       </main>
       <Footer />
     </>
